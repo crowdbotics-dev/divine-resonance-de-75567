98c32807b1c12a6312c7b57851bdefeecbe1b11f
Crowdbotics <engineering@crowdbotics.com>
2023-04-10 01:08:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "divine-resonance-de-75567",
+  url: "https://divine-resonance-de-75567.botics.co",
+  api: "https://divine-resonance-de-75567.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
